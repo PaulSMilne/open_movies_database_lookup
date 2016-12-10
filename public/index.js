933d2c1e30fa55b69fc2471e1a5e9b747d1d2354
Paul Milne <hallhill@gmail.com>
2016-12-10 10:07:14 +0000

    api request successfully written and returns data for movie submitted

--- public/index.js
+++ public/index.js
@@ -1,22 +1,40 @@
 var app = function(){
+
      var submit = document.querySelector("#submit");
-     console.log(submit);
-
-     var searchResults = document.querySelector('#search_results');
-     console.log(searchResults);
-
-     var handleSubmit = function(){
-          console.log("I have been clicked");
-          var pTag = document.createElement("p");
-          console.log(pTag);
-          var searchBox = document.querySelector('#search_box');
-          console.log(searchBox);
-          console.log(searchBox.value);
-          pTag.innerText = searchBox.value;
-          searchResults.appendChild(pTag);
-     }
-
      submit.onclick = handleSubmit;
+
+}
+
+var handleSubmit = function(){
+     var searchBox = document.querySelector("#search_box");
+     var searchTerm = searchBox.value;
+     var url = "http://www.omdbapi.com/?t=" + searchTerm + "&plot=short&r=json";
+     makeRequest(url, requestComplete);
+}
+
+var makeRequest = function(url, callback){
+     var request = new XMLHttpRequest();
+     request.open('GET', url);
+     request.onload = callback;
+     request.send();
+}
+
+var requestComplete = function(){
+     if (this.status !== 200) return;
+     var jsonString = this.responseText;
+     var movie = JSON.parse(jsonString);
+     displayResult(movie);
+}
+
+var displayResult = function(result){
+     var searchResults = document.getElementById("search_results");
+     console.log(searchResults);
+     var poster = document.createElement("img");
+     console.log(poster);
+     console.log(result.Poster);
+     poster.src = result.Poster;
+     console.log(poster.src);
+     searchResults.appendChild(poster);
 }
 
 window.onload = app;
--- readme.md
+++ readme.md
@@ -1,3 +1,23 @@
-This will be a simple movie look up from the OMDB. Input a name and get details about the movie and an image of the poster where it exists.
+# Week 11 weekend homework
 
-It will include a search box with event listeners to pass the title on
+This will be a movie look up from the OMDB. Input a name and get details about the movie and an image of the poster where it exists.
+
+It will include a search box with event listeners to pass the title on.
+
+## MVP
+
+Submit movie name will return a selection of data about the movie:
+
+* Title
+* Year
+* Director
+* Principal actors
+* Plot
+* IMdB rating
+* Poster
+
+## Extensions
+
+* Trailer from YouTube
+* Save movie as favorite in local storage
+* Display favorites in a bar chart showing IMdB rating
