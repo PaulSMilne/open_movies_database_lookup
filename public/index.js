31f3ceff80add8a8277f07b25b937ea61e17c5c1
Paul Milne <hallhill@gmail.com>
2016-12-10 08:57:05 +0000

    wrote input value to screen

--- public/index.js
+++ public/index.js
@@ -1,5 +1,21 @@
 var app = function(){
      var submit = document.querySelector("#submit");
+     console.log(submit);
+
+     var searchResults = document.querySelector('#search_results');
+     console.log(searchResults);
+
+     var handleSubmit = function(){
+          console.log("I have been clicked");
+          var pTag = document.createElement("p");
+          console.log(pTag);
+          var searchBox = document.querySelector('#search_box');
+          console.log(searchBox);
+          console.log(searchBox.value);
+          pTag.innerText = searchBox.value;
+          searchResults.appendChild(pTag);
+     }
+
      submit.onclick = handleSubmit;
 }
 
