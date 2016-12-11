eb18faa5410c65c0553d9ab9f5ac4cdf478fc518
Paul Milne <hallhill@gmail.com>
2016-12-11 16:14:32 +0000

    added a favorites checkbox and para to appear under the results table

--- public/index.js
+++ public/index.js
@@ -122,8 +122,23 @@ var displayResult = function(result){
 
      resultsTable.appendChild(plotRow);
 
+     var favoritesCheck = document.createElement("p");
+
+     var checkBox = document.createElement("input");
+     checkBox.setAttribute("type", "checkbox");
+     checkBox.setAttribute("id", "favoritesCheckBox");
+
+     var checkBoxLabel = document.createElement("label");
+     checkBoxLabel.setAttribute("for", "favoritesCheckBox");
+     checkBoxLabel.innerText = "Favorite";
+
+     favoritesCheck.appendChild(checkBox);
+     favoritesCheck.appendChild(checkBoxLabel);
+
+
      searchResults.appendChild(poster);
      searchResults.appendChild(resultsTable);
+     searchResults.appendChild(favoritesCheck);
 
 }
 
